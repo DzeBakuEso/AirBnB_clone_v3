cefa3a2848fe277519c8de77df9fce725090f315
DzeBakuEso <dzeblebaku@gmail.com>
2025-02-17 22:57:16 +0000

    api/v1/app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,17 +1,22 @@
-from flask import Flask
-from models import storage
+#!/usr/bin/python3
+"""API entry point"""
+
+from flask import Flask, jsonify
 from api.v1.views import app_views
-import os
+from models import storage
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
+@app.errorhandler(404)
+def not_found(error):
+    """Returns a JSON-formatted 404 status code response"""
+    return jsonify({"error": "Not found"}), 404
+
 @app.teardown_appcontext
-def teardown(exception):
-    """Close storage session"""
+def teardown_db(exception):
+    """Closes the storage session"""
     storage.close()
 
 if __name__ == "__main__":
-    host = os.getenv("HBNB_API_HOST", "0.0.0.0")
-    port = int(os.getenv("HBNB_API_PORT", 5000))
-    app.run(host=host, port=port, threaded=True)
+    app.run(host="0.0.0.0", port=5000, threaded=True)
--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,4 +1,4 @@
-from flask import Blueprintmkd
+from flask import Blueprint
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
