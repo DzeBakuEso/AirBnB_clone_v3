1f6a9e5af6b8b334d5fb2a787b582e859803eaed
DzeBakuEso <dzeblebaku@gmail.com>
2025-02-17 23:04:54 +0000

    api/v1/views/states.py, api/v1/views/__init__.py

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,5 +1,9 @@
+#!/usr/bin/python3
+"""Initialize app_views Blueprint"""
+
 from flask import Blueprint
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
-from api.v1.views.index import *  # noqa
+# Import all view modules
+from api.v1.views.states import *
