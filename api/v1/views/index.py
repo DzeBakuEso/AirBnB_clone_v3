7b873bf276e3d8f72e221e899fd83b5532ba97de
DzeBakuEso <dzeblebaku@gmail.com>
2025-02-17 22:53:42 +0000

    api/v1/views/index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,7 +1,19 @@
-from api.v1.views import app_views
-from flask import jsonify
+#!/usr/bin/python3
+""" Index module for API """
 
-@app_views.route('/status', methods=['GET'], strict_slashes=False)
-def status():
-    """Returns API status"""
-    return jsonify(status="OK")
+from flask import jsonify
+from api.v1.views import app_views
+from models import storage
+
+@app_views.route('/stats', methods=['GET'])
+def get_stats():
+    """Retrieve the number of each object by type"""
+    stats = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User")
+    }
+    return jsonify(stats)
